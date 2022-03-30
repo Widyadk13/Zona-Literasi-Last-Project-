15db708e85bf35326975ceacb8434a2e6bedc5b7
Dandyndot29 <dandysetiawanptr@gmail.com>
2022-03-30 22:55:58 +0800

    add booklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Bookslist from './components/booksList';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Bookslist/>
     </>
   );
 }
