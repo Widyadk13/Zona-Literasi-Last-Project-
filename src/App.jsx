784a6f24f5b0adb6a747811252f0ca08d6c81f22
Dandyndot29 <dandysetiawanptr@gmail.com>
2022-03-30 23:38:42 +0800

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Bookslist from './components/BooksList';
+import Header from './components/Header';
 import Footer from './components/Footer';
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Header/>
       <Bookslist/>
       <Footer/>
     </div>
